386b6163c06723fa7f8c041e21f760938167e6b1
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2022-11-24 15:58:58 -0300

    commit init

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var calcular = function()  {    
+    let cantidad = parseInt(document.querySelector("#cantidad").value);
+    let color = document.querySelector("#color").value;
+    document.querySelector("#totaltotal").innerHTML = 
+    `<div class="card main-bg">               
+      <ul class="list-group list-group-flush p-4>
+        <li class="list-group-item">Total= $${cantidad * 40000}</li>
+        <li class="list-group-item">Cantidad= ${cantidad}</li>
+        <li class="list-group-item">Color= <span 
+          style="height: 25px; width: 25px; border-radius: 50%; background-color: ${color}; display: inline-block">  
+        <span/></li>
+      </ul>       
+    </div>`;
+    
+  };
